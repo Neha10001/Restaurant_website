1c4b7158cd4d9493f07b2de778ab0cf8547f2d4c
Neha <akula.neha2020@vitstudent.ac.in>
2023-06-13 16:50:58 +0530

    React project

---
+++ Project/restaurant-website/src/App.js
@@ -0,0 +1,12 @@
+import Layout from "./components/Layout/Layout";
+
+function App() {
+  return (
+    <div>
+      <h1>Restaurant Website</h1>
+      <Layout />
+    </div>
+  );
+}
+
+export default App;
